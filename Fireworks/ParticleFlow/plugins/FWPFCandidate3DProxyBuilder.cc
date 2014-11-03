6584ca50a8d864994f358a2ecc9816b783752627
vazzolini <v.azzolini@gmail.com>
2014-11-03 17:07:48 +0100

    Still XRays. changed weighting function

--- Fireworks/ParticleFlow/plugins/FWPFCandidate3DProxyBuilder.cc
+++ Fireworks/ParticleFlow/plugins/FWPFCandidate3DProxyBuilder.cc
@@ -40,6 +40,7 @@
 #include "DataFormats/HGCRecHit/interface/HGCRecHitCollections.h"
 #include "Fireworks/Core/interface/fwLog.h"
 #include "DataFormats/DetId/interface/DetId.h"
+
 //-----------------------------------------------------------------------------
 // FWPFCandidate3DProxyBuilder
 //-----------------------------------------------------------------------------
@@ -65,8 +66,9 @@ private:
   const FWPFCandidate3DProxyBuilder& operator=( const FWPFCandidate3DProxyBuilder& );   // Stop default
 
   // --------------------- Member Functions --------------------------
-  void build( const reco::PFCandidate& iData, unsigned int iIndex, TEveElement& oItemHolder, const FWViewContext* );
+  void build( const reco::PFCandidate& iData, unsigned int iIndex, TEveElement& oItemHolder, const FWViewContext* ); //default
   void digitColor(TEveBoxSet* bs, float v, float max);
+  void digitColor255(TEveBoxSet* bs, float v, float max);
 };
 //=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_
 
@@ -75,10 +77,8 @@ FWPFCandidate3DProxyBuilder::~FWPFCandidate3DProxyBuilder(){}
 
 //______________________________________________________________________________
 void 
-FWPFCandidate3DProxyBuilder::build( const reco::PFCandidate& iData, unsigned int iIndex, TEveElement& oItemHolder, const FWViewContext* ) 
+FWPFCandidate3DProxyBuilder::build( const reco::PFCandidate& iData, unsigned int iIndex, TEveElement& oItemHolder, const FWViewContext* ) //default
 {
-
-  if (iIndex)  return;
 
   //  TEveCompound* comp = createCompound(false,true);  
   TEveElement* comp = &oItemHolder;
@@ -90,25 +90,24 @@ FWPFCandidate3DProxyBuilder::build( const reco::PFCandidate& iData, unsigned int
   // ref HGC collections
   edm::Handle<HGCRecHitCollection> m_collectionHGC;
   HGCRecHitCollection::const_iterator rechit;
-  //std::map<DetId, HGCRecHitCollection::const_iterator> inversemap;
   edm::InputTag tagEE("HGCalRecHit:HGCEERecHits");
   edm::InputTag tagHEF("HGCalRecHit:HGCHEFRecHits");
   edm::InputTag tagHEB("HGCalRecHit:HGCHEBRecHits");
 
   const auto* theEvent = item()->getEvent();
-  fwLog(fwlog::kError) << "---------------------------------------------------------------------Event: "<< theEvent->id().event() << std::endl; 
+  //fwLog(fwlog::kError) << "1 ---------------------------------------------------------------------Event: "<< theEvent->id().event() << "-------------------"<<std::endl; 
   
   // // look for max and min energy cluster --------
   float energy_max=0;
   float energy_maxEE=0;
   float energy_maxHEF=0;
   float energy_maxHEB=0;
+
+  //float energy_maxEvent=0;
   // ---/. XRays -------------------------------------
   
   for( unsigned i = 0 ; i < elems.size(); ++i ) {
     const reco::PFBlockElement& elem = elems[i].first->elements()[elems[i].second];
-
-    fwLog(fwlog::kError) << "+++++++++++++++++++++++++++ PFBlock index " << elem.clusterRef().index() << " +++++++++++++++++++++++++++++"<< std::endl; // XRays
 
     switch( elem.type() ) {
     case reco::PFBlockElement::TRACK:
@@ -131,7 +130,6 @@ FWPFCandidate3DProxyBuilder::build( const reco::PFCandidate& iData, unsigned int
     case reco::PFBlockElement::HGC_HCALB:
       { 
 	if( elem.clusterRef().isNull() || !elem.clusterRef().isAvailable() ) {
-	  fwLog(fwlog::kError) << " IN 1" << "  elem.clusterRef().isNull || not elem.clusterRef().isAvailable"<<  std::endl;// XRays
 	  TEveRecTrack t;
 	  t.fBeta = 1.;
 	  t.fP = TEveVector( iData.px(), iData.py(), iData.pz() );
@@ -143,17 +141,12 @@ FWPFCandidate3DProxyBuilder::build( const reco::PFCandidate& iData, unsigned int
 	  setupAddElement( trk, comp, false );
 	  continue;
 	}
-	fwLog(fwlog::kError) << " IN 2" << std::endl;// XRays
 
 	const std::vector<std::pair<DetId, float> >& clusterDetIds = 
 	  elem.clusterRef()->hitsAndFractions();
 	TEveBoxSet* boxset = new TEveBoxSet(); // def
 	boxset->Reset(TEveBoxSet::kBT_FreeBox, true, clusterDetIds.size()); // def
 	boxset->SetMainTransparency(1);       // XRays 
-
-
-
-	fwLog(fwlog::kError) << "+++++++++++++++++++++++++++ PFBlock index " << elem.clusterRef().index() << "  clusterDetIds size " <<  clusterDetIds.size() <<" +++++++++++++++++++++++++++"<< std::endl;//XRays
 
 	// --- XRays -------------------------------------
 	// --- look for RecHit maximum energy within a cluster, -------------------
@@ -164,23 +157,22 @@ FWPFCandidate3DProxyBuilder::build( const reco::PFCandidate& iData, unsigned int
 	  theEvent->getByLabel(tagEE,  m_collectionHGC); 
 	  energy_maxEE=0; 
 	  ta = 0;
-	  fwLog(fwlog::kError) << "IN 4-0 " << ta << " HGCEE  "<< std::endl;
 	  break;
 	case reco::PFBlockElement::HGC_HCALF:
 	  theEvent->getByLabel(tagHEF, m_collectionHGC); 
 	  energy_maxHEF=0;
 	  ta = 1;
-	  fwLog(fwlog::kError) << "IN 4-1 " << ta << " HGCHEF "<< std::endl;
 	  break;            
 	case reco::PFBlockElement::HGC_HCALB:
 	  theEvent->getByLabel(tagHEB, m_collectionHGC);
 	  energy_maxHEB=0; 
 	  ta = 2;
-	  fwLog(fwlog::kError) << "IN 4-2 " << ta << " HGCHEB "<< std::endl;
 	  break;
 	default:
 	  break;
-	}
+	} // elem.type
+	
+	if (! m_collectionHGC.product()) continue;
 
 	boxset->SetName(Form("BOX_%d", ta));   
         
@@ -189,49 +181,22 @@ FWPFCandidate3DProxyBuilder::build( const reco::PFCandidate& iData, unsigned int
 	  {
 	    auto iter = m_collectionHGC->find(ip->first);             
 	    if( iter == m_collectionHGC->end() ) continue;
-	    //fwLog(fwlog::kError) << " __________________________________________ IN 5 event number "   << m_collectionHGC->find(ip->first) << " ta "<< ta << std::endl;
 	    Float_t energy  = iter->energy();
-            
-	    // fwLog(fwlog::kError) <<" 0) Pre-loop:            " <<
-	    //   "   index " << elem.clusterRef().index() << ", size " <<  clusterDetIds.size()<< ",detector "<< ta <<
-	    //   " energy_max " << energy_max << " ---? < "<<" energy of that ClusterIds " << energy     <<  std::endl;
             
 	    if (energy > energy_max){
 	      energy_max = energy;
-                
-	      fwLog(fwlog::kError) <<"   1) Inside Loop for max: " <<
-		" index " << elem.clusterRef().index() << ", size " <<  clusterDetIds.size()<< ",detector "<< ta <<
-		" energy_max " << energy_max << " == "<<" energy " << energy     <<  "<----------------------------"<< std::endl;
-                
 	    }
             
 	  }// /. ip //out of this loop energy_max and energy_min are  the maximum and min energy of the cluster 
 
-	if(ta==0){         energy_maxEE  = energy_max; 
-	  fwLog(fwlog::kError) <<" 2) Outside Loop for max: Found the max" << " index " << elem.clusterRef().index() << ", size " <<  clusterDetIds.size()<< ",detector "<< ta << " energy_maxEE " <<  energy_maxEE<<"-"<<energy_max << " == "  <<  std::endl;
-	  energy_max=0;
-	} else if (ta==1){ energy_maxHEF = energy_max;
-	  fwLog(fwlog::kError) <<" 2) Outside Loop for max: Found the max" << " index " << elem.clusterRef().index() << ", size " <<  clusterDetIds.size()<< ",detector "<< ta <<  " energy_maxHEF " <<  energy_maxHEF<<"-"<<energy_max << " == "<<  std::endl;
-	  energy_max=0;
-	} else if (ta==2){ energy_maxHEB = energy_max;
-	  fwLog(fwlog::kError) <<" 2) Outside Loop for max: Found the max" << " index " << elem.clusterRef().index() << ", size " <<  clusterDetIds.size()<< ",detector "<< ta <<  " energy_maxHEB " <<  energy_maxHEB<<"-"<<energy_max << " == "<<  std::endl; 
-	  energy_max=0;
+	if(ta==0){         energy_maxEE  = energy_max;   energy_max=0;
+	} else if (ta==1){ energy_maxHEF = energy_max;   energy_max=0;
+	} else if (ta==2){ energy_maxHEB = energy_max;   energy_max=0;
 	}
 
-	fwLog(fwlog::kError) <<std::endl;
-	fwLog(fwlog::kError) <<std::endl;
-	fwLog(fwlog::kError) <<" --------- > at the end of the triple Loop "<<
-	  " index " <<          elem.clusterRef().index() << 
-	  ", size " <<          clusterDetIds.size()<< 
-	  " energy_max EE  " << energy_maxEE  << " && " <<
-	  " energy_max HEF " << energy_maxHEF << " && " <<
-	  " energy_max HEB " << energy_maxHEB << "    ta  "<< ta<<  
-	  " ------------------------ "<< std::endl;
-        
-	// --- GOT them, an energy max for each subdet -----------------------
+	// --- Got them, a different energy max value for each subdet -----------------------
 	// --- /. XRays -------------------------------------
 
-	//for( unsigned tb = 0 ; tb <= 2; ++tb ) {      
 	for( std::vector<std::pair<DetId, float> >::const_iterator it = clusterDetIds.begin(), itEnd = clusterDetIds.end();
 	     it != itEnd; ++it) 
 	  {
@@ -244,66 +209,36 @@ FWPFCandidate3DProxyBuilder::build( const reco::PFCandidate& iData, unsigned int
 	    boxset->AddBox( &pnts[0]);
 
 	    // --- XRays -------------------------------------
-	    auto iter = m_collectionHGC->find(it->first);
-	    if( iter == m_collectionHGC->end() ) continue;
+	    auto iterA = m_collectionHGC->find(it->first);
+	    if( iterA == m_collectionHGC->end() ) continue;
 
-	    Float_t energy2  = iter->energy();
-	    if(energy2 ==0){fwLog(fwlog::kError) <<" wuuuuuuuuuuhuuuuuuuuuuuuuuuuuu    rechit energy2 is zero "  << " --- det " <<ta << std::endl;}
+	    Float_t energy2  = iterA->energy();
             
-	    //  digitColor(boxset, energy_maxEE, energy2); // default
 	    if(ta == 0 ){//-------------------------------------------------------------
-	      digitColor(boxset, energy2, energy_maxEE);
 	      if(energy2 == energy_maxEE){  
-		
-		fwLog(fwlog::kError) <<
-		  "================================= Refound recHit max!  " <<           
-		  "  index "        << elem.clusterRef().index() << 
-		  ", size "         << clusterDetIds.size()<< ",detector "<< ta <<
-		  "  energy_maxEE " << energy_maxEE << " ---? < "<<
-		  "  energy of that ClusterIds " << energy2     <<  std::endl;
+		digitColor255(boxset, energy_maxEE, energy_maxEE);
 	      }else{
-		fwLog(fwlog::kError) <<	  "================================= Smaller brothers def"         << 
-		  "255*energy2/energy_maxEE " << "255*"<< energy2 <<"/"<< energy_maxEE  << " :  "<< 255*energy2/energy_maxEE <<std::endl;
+		digitColor(boxset, energy2, energy_maxEE);
 	      }
-	    }               
-	    else if(ta == 1){//-------------------------------------------------------------
-	      digitColor( boxset, energy2, energy_maxHEF);
+	    }              
+	    else if(ta == 1){//---------------------------------------------------------
 	      if(energy2 == energy_maxHEF){ 
-		// fwLog(fwlog::kError) <<
-		//   "================================= Refound recHit max!  " <<           
-		//   "  index "        << elem.clusterRef().index() << 
-		//   ", size "         <<  clusterDetIds.size()<< ",detector "<< ta <<
-		//   "  energy_maxHEF "<< energy_maxHEF << " ---? < "<<
-		//   "  energy of that ClusterIds " << energy2     <<  std::endl;
+		digitColor255(boxset, energy_maxHEF, energy_maxHEF);
 	      } else{
-		// fwLog(fwlog::kError) <<
-		//   "================================= Smaller brothers "         << 
-		//   "255*energy2/energy_maxHEF " << 
-		//   "255*"<< energy2 <<"/"<< energy_maxHEF  << " :  "<< 255*energy2/energy_maxHEF <<std::endl;
+		digitColor( boxset, energy2, energy_maxHEF);
 	      }
 	    }
-	    else if(ta == 2){//-------------------------------------------------------------
+	    else if(ta == 2){//---------------------------------------------------------
 	      if (energy2 == energy_maxHEB){
-		digitColor(boxset, energy2, energy_maxHEB);
-		// fwLog(fwlog::kError) <<
-		//   "================================= Refound recHit max!  " <<           
-		//   "  index "        << elem.clusterRef().index() << 
-		//   ", size "         << clusterDetIds.size()<< ",detector "<< ta <<
-		//   "  energy_maxHEB "<< energy_maxHEB << " ---? < "<<
-		//   "  energy of that ClusterIds " << energy2     <<  std::endl;
+		digitColor255(boxset, energy_maxHEB, energy_maxHEB);
 	      } else{
-		// fwLog(fwlog::kError) <<
-		//   "================================= Smaller brothers "         << 
-		//   "255*energy2/energy_maxHEB " << 
-		//   "255*"<< energy2 <<"/"<< energy_maxHEB  << " :  "<< 255*energy2/energy_maxHEB <<std::endl;
+		digitColor(boxset, energy2, energy_maxHEB);
 	      }
 	    }
-               
-                  
 	  } // /. it
             // --- /. XRays -------------------------------------
-              
-	boxset->RefitPlex(); //Instruct underlying memory allocator to regroup itself into a contiguous memory chunk.
+	
+	boxset->RefitPlex();                 //Instruct underlying memory allocator to regroup itself into a contiguous memory chunk.
 	setupAddElement(boxset,comp, false); //(element, parents, color)
 
 	// /.cases
@@ -312,11 +247,10 @@ FWPFCandidate3DProxyBuilder::build( const reco::PFCandidate& iData, unsigned int
     default:
       break;
     } // /. of switch(elem.type) // type of PF candidate 
-  } // /. elem.size // size of the PF candidate
+  } // /. elems.size         // size of the PF candidates
   
-  //  comp->SetMainColor((unsigned)2.0*myRandom.Uniform(50));
-  
-  // setupAddElement( comp, &oItemHolder, false );
+  //comp->SetMainColor((unsigned)2.0*myRandom.Uniform(50));
+  //setupAddElement( comp, &oItemHolder, false );
 }
 
 
@@ -327,14 +261,27 @@ void FWPFCandidate3DProxyBuilder::digitColor(TEveBoxSet* bs, float v, float max)
   bs->SetMainTransparency(1);
   // last argument is opacitiy [0-255]
   if (item())
-    //  bs->DigitColor(item()->defaultDisplayProperties().color(), 255*v/max);             //def
-    //  bs->DigitColor(item()->defaultDisplayProperties().color(), 255*exp( -v/max + 1 )); //idea 1 to enphasize the difference between rechits
-    //  bs->DigitColor(item()->defaultDisplayProperties().color(), 255*exp( -v/max  ));    //idea 2
-    //  bs->DigitColor(item()->defaultDisplayProperties().color(), 255*(max - v)/ max);    //idea 3
-    bs->DigitColor(item()->defaultDisplayProperties().color(), 255*exp(1-max/v));      // idea 4
+    //  bs->DigitColor(item()->defaultDisplayProperties().color(), 255*v/max);             // def
+    //  bs->DigitColor(item()->defaultDisplayProperties().color(), 255*exp( -v/max + 1 )); // idea 1
+    //  bs->DigitColor(item()->defaultDisplayProperties().color(), 255*exp( -v/max  ));    // idea 2
+    bs->DigitColor(item()->defaultDisplayProperties().color(), 255*(max - v)/ max);        // idea 3 <------
+  //  bs->DigitColor(item()->defaultDisplayProperties().color(), 255*exp(1-max/v));      // idea 4
+  //  bs->DigitColor(item()->defaultDisplayProperties().color(), 255*pow(v/max,2));      // idea 5
 }
 
 //______________________________________________________________________________
+//______________________________________________________________________________
+
+void FWPFCandidate3DProxyBuilder::digitColor255(TEveBoxSet* bs, float v, float max) {
+
+  bs->SetMainTransparency(1);
+  // last argument is opacitiy [0-255]
+  if (item())
+    bs->DigitColor(item()->defaultDisplayProperties().color(), 255);                   // def <---
+}
+
+//______________________________________________________________________________
+
 
 
 
@@ -344,7 +291,8 @@ FWPFCandidate3DProxyBuilder::localModelChanges(const FWModelId& iId, TEveElement
                                                FWViewType::EType viewType, const FWViewContext* vc)
 {
   UChar_t rgba[4];
-  TEveUtil::ColorFromIdx(item()->defaultDisplayProperties().color(), rgba, item()->defaultDisplayProperties().transparency());
+  TEveUtil::ColorFromIdx(item()->defaultDisplayProperties().color() + context().colorManager()->offsetOfLimitedColors(), rgba, item()->defaultDisplayProperties().transparency()); 
+  // TEveUtil::ColorFromIdx(item()->defaultDisplayProperties().color(), rgba, item()->defaultDisplayProperties().transparency()); // def
   // TEveUtil::ColorFromIdx(Color_t ci, UChar_t col[4], UChar_t transparency) 
   // this takes color-index (Short_t, can be translated to RGB) and transparency 
   // and converts them to RGBA representation (A ~ alpha value as accepted by GL: 0 - fully transparent, 255 - fully opaque)
@@ -367,6 +315,8 @@ FWPFCandidate3DProxyBuilder::localModelChanges(const FWModelId& iId, TEveElement
     }
 
 }
+//    bs->DigitColor(item()->defaultDisplayProperties().color() + context().colorManager()->offsetOfLimitedColors() , 255);
+//comp->SetMainColor((unsigned)2.0*myRandom.Uniform(50));
 
 //______________________________________________________________________________
 REGISTER_FWPROXYBUILDER(FWPFCandidate3DProxyBuilder, reco::PFCandidate,"PF Candidates", FWViewType::kAll3DBits | FWViewType::kAllRPZBits );
